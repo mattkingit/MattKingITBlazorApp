c9cf1b475d094fbd25c71d76b7ebdac8b556e7fa
Matthew L King <4024-matthew.l.king.ctr@users.noreply.gitlab.devforce.disa.mil>
2021-09-22 11:28:07 -0500

    Commit this ...

--- obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace MKBlazorApp.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Code\Github\MattKingITBlazorApp\_Imports.razor"
+#line 1 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Code\Github\MattKingITBlazorApp\_Imports.razor"
+#line 2 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Code\Github\MattKingITBlazorApp\_Imports.razor"
+#line 3 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Code\Github\MattKingITBlazorApp\_Imports.razor"
+#line 4 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Code\Github\MattKingITBlazorApp\_Imports.razor"
+#line 5 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Code\Github\MattKingITBlazorApp\_Imports.razor"
+#line 6 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Code\Github\MattKingITBlazorApp\_Imports.razor"
+#line 7 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Code\Github\MattKingITBlazorApp\_Imports.razor"
+#line 8 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Code\Github\MattKingITBlazorApp\_Imports.razor"
+#line 9 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\_Imports.razor"
 using MKBlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Code\Github\MattKingITBlazorApp\_Imports.razor"
+#line 10 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\_Imports.razor"
 using MKBlazorApp.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using MKBlazorApp.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 48 "C:\Code\Github\MattKingITBlazorApp\Shared\NavMenu.razor"
+#line 48 "C:\Users\matthew.l.king\source\repos\MKBlazorApp\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
