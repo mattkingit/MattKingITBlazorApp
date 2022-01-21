1941030135a5ba438e5d8edcadb29fd95ff11fbc
Matthew King <matthew.l.king.ctr@army.mil>
2022-01-21 13:31:34 -0600

    Update

--- obj/Debug/net5.0/RazorDeclaration/Pages/Blog.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Blog.razor.g.cs
@@ -13,70 +13,70 @@ namespace MKBlazorApp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\code\dotnet\Blazor\MattKingITBlazorApp\_Imports.razor"
+#line 1 "C:\Users\matthew.l.king\Source\Repos\mattkingit\MattKingITBlazorApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\code\dotnet\Blazor\MattKingITBlazorApp\_Imports.razor"
+#line 2 "C:\Users\matthew.l.king\Source\Repos\mattkingit\MattKingITBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\code\dotnet\Blazor\MattKingITBlazorApp\_Imports.razor"
+#line 3 "C:\Users\matthew.l.king\Source\Repos\mattkingit\MattKingITBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\code\dotnet\Blazor\MattKingITBlazorApp\_Imports.razor"
+#line 4 "C:\Users\matthew.l.king\Source\Repos\mattkingit\MattKingITBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\code\dotnet\Blazor\MattKingITBlazorApp\_Imports.razor"
+#line 5 "C:\Users\matthew.l.king\Source\Repos\mattkingit\MattKingITBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\code\dotnet\Blazor\MattKingITBlazorApp\_Imports.razor"
+#line 6 "C:\Users\matthew.l.king\Source\Repos\mattkingit\MattKingITBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\code\dotnet\Blazor\MattKingITBlazorApp\_Imports.razor"
+#line 7 "C:\Users\matthew.l.king\Source\Repos\mattkingit\MattKingITBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\code\dotnet\Blazor\MattKingITBlazorApp\_Imports.razor"
+#line 8 "C:\Users\matthew.l.king\Source\Repos\mattkingit\MattKingITBlazorApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\code\dotnet\Blazor\MattKingITBlazorApp\_Imports.razor"
+#line 9 "C:\Users\matthew.l.king\Source\Repos\mattkingit\MattKingITBlazorApp\_Imports.razor"
 using MKBlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\code\dotnet\Blazor\MattKingITBlazorApp\_Imports.razor"
+#line 10 "C:\Users\matthew.l.king\Source\Repos\mattkingit\MattKingITBlazorApp\_Imports.razor"
 using MKBlazorApp.Shared;
 
 #line default
